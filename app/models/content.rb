fb26b76452c711d9f77487b0c85db3a3dd75c6ef
moznion <moznion@gmail.com>
2013-09-15 00:01:54 +0900

    Remove redundant features.

--- app/models/content.rb
+++ app/models/content.rb
@@ -3,8 +3,6 @@ class Content < ActiveRecord::Base
 
   has_attached_file :body, {
     :styles => {
-      :thumb  => "50x50>",
-      :medium => "100x100>",
       :large  => "300x300>"
     }
   }
