d11e48cdefef7e1a6f5b4410252f1457932801cd
moznion <moznion@gmail.com>
2013-09-14 23:11:32 +0900

    Revert "画像アップロード機能のコア部分を実装"
    This reverts commit 234d7ac056aa0b36dfca939db660788553dfb440.

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -62,13 +62,13 @@ class ContentsController < ApplicationController
   end
 
   private
-  # Use callbacks to share common setup or constraints between actions.
-  def set_content
-    @content = Content.find(params[:id])
-  end
+    # Use callbacks to share common setup or constraints between actions.
+    def set_content
+      @content = Content.find(params[:id])
+    end
 
-  # Never trust parameters from the scary internet, only allow the white list through.
-  def content_params
-    params.require(:content).permit(:name, :category, :mime, :last_accessed_at, :created_at, :updated_at, :body)
-  end
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def content_params
+      params[:content]
+    end
 end
