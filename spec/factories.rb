aced223ad5b07ce20ac371e9144de1d98c938eb5
moznion <moznion@gmail.com>
2013-09-15 22:28:45 +0900

    Factories を移動

---
+++ spec/factories.rb
@@ -0,0 +1,33 @@
+include ActionDispatch::TestProcess
+
+FactoryGirl.define do
+  tempfile             = Tempfile.new('valid', [Rails.root, 'spec', 'fixtures', 'images'].join("/"))
+  too_large_tempfile = Tempfile.new('too_large', [Rails.root, 'spec', 'fixtures', 'images'].join("/"))
+
+  str = ''
+  (1024 * 1024 * 5 + 1).times do
+    str << 'a'
+  end
+  too_large_tempfile.print str
+
+  factory :valid_jpg_content, :class => Content do |c|
+    c.body { fixture_file_upload(tempfile.path, "image/jpeg") }
+  end
+
+  factory :valid_png_content, :class => Content do |c|
+    c.body { fixture_file_upload(tempfile.path, "image/png") }
+  end
+
+  factory :valid_gif_content, :class => Content do |c|
+    c.body { fixture_file_upload(tempfile.path, "image/gif") }
+  end
+
+  factory :invalid_file_type_content, :class => Content do |c|
+    c.body { fixture_file_upload(tempfile.path, "text/plain") }
+  end
+
+  factory :invalid_too_large_content, :class => Content do |c|
+    c.body { fixture_file_upload(too_large_tempfile.path, "image/jpeg") }
+  end
+end
+
