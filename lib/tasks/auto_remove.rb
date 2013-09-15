9aaea7ba5741da70658ef35c0b6246bca060143e
moznion <moznion@gmail.com>
2013-09-15 16:49:06 +0900

    削除処理をちょっと効率的にした

--- lib/tasks/auto_remove.rb
+++ lib/tasks/auto_remove.rb
@@ -1,11 +1,19 @@
 class Tasks::AutoRemove
   def self.execute
-    now_epoch = Time.now.gmtime.to_i
-    delta_time_epoch = 86400 # <= 24 hours
-    Content.all.each do |content|
+    now_epoch            = Time.now.gmtime.to_i
+    delta_time_epoch     = 86400 # <= 24 hours
+    out_dated_following  = false
+
+    Content.order('last_accessed_at DESC').each do |content|
+      if out_dated_following
+        content.destroy
+        next
+      end
+
       last_accessed_at_epoch = content.last_accessed_at.gmtime.to_i
       if now_epoch - last_accessed_at_epoch > delta_time_epoch
         content.destroy
+        out_dated_following = true
       end
     end
   end
