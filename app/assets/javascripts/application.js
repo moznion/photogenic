d598b801b9929a4f72aa3275fbde019d12cf972b
Takuma Nishimura <tak.nishimura.0723@gmail.com>
2013-09-15 13:00:35 +0900

    どろっぷぞーん
    ドロップゾーンとホバーの動きをちょっとかいときました。

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,12 +18,27 @@
 
 $(function(){
 	//dropzone 設定
+	$("#logo a img").hover(
+		function(){
+			$(this).fadeTo(100,0.6);
+		},
+		function(){
+			$(this).fadeTo(100,1);
+		}
+	);
 	$(".new_content").addClass("dropzone-custom");
-	$("div#dropArea").dropzone({
-		url: "contents/new",
-		parallelUploads:1,
-		maxFilesize:5,
-		clickable:true,
 
-	});
+
+	//ドロップゾーン　ここから
+
+	//ドロップゾーン　ここまで
+
+	$(".dropzone-custom").hover(
+		function(){
+			$(this).css({"background":"#dcdcdc"},200);
+	},
+		function(){
+			$(this).css({"background":"#FFFFFF"},200);
+		}
+	);
 });
